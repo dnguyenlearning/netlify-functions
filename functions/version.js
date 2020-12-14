8e67fdac66d659c9f182556d100a1686dbeb1a99
Duong Nguyen <dng@localizedirect.com>
2020-12-14 03:07:23 -0500

    test

--- functions/version.js
+++ functions/version.js
@@ -1,8 +1,9 @@
 const VersionNumber = require("./versionNumber");
+const pkjson = require("../package.json");
 
 exports.handler = async (event, context, callback) => {
   return {
     statusCode: 200,
-    body: VersionNumber,
+    body: "Hello a Lynh " + pkjson.version,
   };
 };
