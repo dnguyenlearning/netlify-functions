2f9cc7a620beeab317eb127b03fc4e7478c8d74d
Duong Nguyen <dng@localizedirect.com>
2020-12-14 03:03:25 -0500

    test

--- functions/version.js
+++ functions/version.js
@@ -1,17 +1,8 @@
-const fs = require("fs");
+const VersionNumber = require("./versionNumber");
 
 exports.handler = async (event, context, callback) => {
-  try {
-    const packageJSON = await fs.readFileSync(__dirname + "/package", "utf8");
-
-    return {
-      statusCode: 200,
-      body: packageJSON?.version,
-    };
-  } catch (err) {
-    return {
-      statusCode: 400,
-      body: "Failed",
-    };
-  }
+  return {
+    statusCode: 200,
+    body: VersionNumber,
+  };
 };
