1c415aab82832cfe592d143e3c97609d082a09ab
Duong Nguyen <dng@localizedirect.com>
2020-12-14 02:38:42 -0500

    test

--- functions/version.js
+++ functions/version.js
@@ -1,21 +1,17 @@
 var readJson = require("read-package-json");
+const fs = require("fs");
 
 exports.handler = async (event, context, callback) => {
   try {
-    readJson("../package.json", console.error, false, function (er, data) {
-      if (er) {
-        return {
-          statusCode: 400,
-          body: "Wrong",
-        };
-      }
+    const packageJSON = await fs.readFileSync(
+      __dirname + "/../package.json",
+      "utf8"
+    );
 
-      console.log("data", data);
-      return {
-        statusCode: 200,
-        body: "0.0.1",
-      };
-    });
+    return {
+      statusCode: 200,
+      body: packageJSON?.version,
+    };
   } catch (err) {
     return {
       statusCode: 400,
