deb13cf31944abf3cbb3bd27b50316decad9ad22
Duong Nguyen <dng@localizedirect.com>
2020-12-14 00:01:46 -0500

    .

--- functions/version.js
+++ functions/version.js
@@ -1,8 +1,9 @@
-import packageJSON from "../package.json";
+const packageJson = require("../package.json");
 
 exports.handler = async (event) => {
+  console.log("packageJson", packageJson);
   console.log("event", event);
-  const version = packageJSON?.version;
+  const version = packageJson?.version;
   return {
     statusCode: 200,
     body: version,
